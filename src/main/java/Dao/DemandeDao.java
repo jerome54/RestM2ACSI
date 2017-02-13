3a176d2f494bb27cf8011e03a58f4b037aa88671
jerome54 <jerome.rollinger@gmail.com>
2017-02-13 17:25:36 +0100

    ajout des classes pour la demande

---
+++ src/main/java/Dao/DemandeDao.java
@@ -0,0 +1,13 @@
+package Dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import Model.Demande;
+
+public interface DemandeDao extends JpaRepository<Demande, Long>  {
+	
+}
+	
+	
+		
+	
